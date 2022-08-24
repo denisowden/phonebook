353c773fc2ebaaa4bf3757176972bac78c46dac7
Denis Denisow <d.osetrov1720@gmail.com>
2022-08-24 20:26:53 +0300

    Commit with Showbook 1.1

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,2 +1,27 @@
-package org.example;public class Book {
+package org.example;
+
+public class Book {
+    private static String name;
+    private static String surname;
+    private static long number;
+
+    public static void setName(String name) {
+        Book.name = name;
+    }
+
+    public static void setSurname(String surname) {
+        Book.surname = surname;
+    }
+
+    public static void setNumber(long number) {
+        Book.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + "\n" +
+                "Фамилия: " + surname + "\n" +
+                "Номер: " + number + "\n" +
+                " ";
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+
+    private static boolean execute;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Showbook.showbook();
     }
 }
