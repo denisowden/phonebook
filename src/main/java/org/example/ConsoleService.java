bc49ed62f4802d35602ff9d988519bb554dfb489
Denis Denisow <d.osetrov1720@gmail.com>
2022-08-25 19:42:29 +0300

    Pre-final version

--- src/main/java/org/example/ConsoleService.java
+++ src/main/java/org/example/ConsoleService.java
@@ -3,5 +3,39 @@ package org.example;
 import java.util.Scanner;
 
 public class ConsoleService {
-    public static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String readLine() {
+        return scanner.nextLine();
+    }
+
+    public static long readNumber() {
+        long a;
+        while (true) {
+            try {
+                a = Long.parseLong(readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Only numbers are accepted");
+            }
+        }
+        return a;
+    }
+
+    public static void start() {
+        while (scanner.hasNext()) {
+            String a = scanner.nextLine();
+            if (a.equalsIgnoreCase("add")) {
+                ContactService.add();
+            } else if (a.equalsIgnoreCase("remove")) {
+                ContactService.remove();
+            } else if (a.equalsIgnoreCase("show")) {
+                ContactService.showbook();
+            } else if (a.equalsIgnoreCase("exit")) {
+                break;
+            } else {
+                System.out.println("Wrong command");
+            }
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,6 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-
-        while (ConsoleService.scanner.hasNext()){
-            String a = ConsoleService.scanner.nextLine();
-            if(a.equalsIgnoreCase("add")){
-                ContactService.add();
-            }else if(a.equalsIgnoreCase("remove")){
-                ContactService.remove();
-            }else if(a.equalsIgnoreCase("show")){
-                ContactService.showbook();
-            }else if(a.equalsIgnoreCase("exit")){
-                break;
-            }
-            else {
-                System.out.println("Wrong command");
-            }
-        }
+        ConsoleService.start();
     }
 }
