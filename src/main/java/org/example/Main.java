96a63e817cbf16e0654623e52ce7da6caada13df
Denis Denisow <d.osetrov1720@gmail.com>
2022-08-25 10:39:49 +0300

    Deleting a contact from the console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,10 @@
 package org.example;
 
-import org.example.util.ConnectionManager;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
-        Showbook.showbook();
-        AddContact.add();
+
 
 
     }
