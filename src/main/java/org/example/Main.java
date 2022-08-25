3a4b5335e173d41a7e90d8d845ffa2dddbab725d
Denis Denisow <d.osetrov1720@gmail.com>
2022-08-25 10:58:31 +0300

    Working with console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,29 @@
 package org.example;
 
 
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (scanner.hasNext()){
+            String a = scanner.nextLine();
+            if(a.equalsIgnoreCase("add")){
+                AddContact.add();
+            }else if(a.equalsIgnoreCase("remove")){
+                DeleteContact.remove();
+            }else if(a.equalsIgnoreCase("show")){
+                Showbook.showbook();
+            }else if(a.equalsIgnoreCase("exit")){
+                break;
+            }else {
+                System.out.println("Wrong command");
+                continue;
+            }
+        }
 
 
     }
